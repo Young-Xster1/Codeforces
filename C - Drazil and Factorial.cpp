09ff9a39da21c3055c5d13d52bb20b06df98707e
Kamel Rmeda <156197102+Young-Xster1@users.noreply.github.com>
2024-02-25 19:22:13 +0100

    Create C - Drazil and Factorial.cpp

---
+++ C - Drazil and Factorial.cpp
@@ -0,0 +1,75 @@
+//* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ///*/
+ 
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using i64 = long long;
+#define pb push_back
+#define fast cout.tie(0), cin.tie(0), ios_base::sync_with_stdio(0)
+#define ll long long
+#define vt vector<ll>
+#define matrix vector<vector<ll>>
+#define ff first
+#define ss second
+#define yes cout << "YES" << '\n';
+#define no cout << "NO" << '\n';
+#define ps(x, y) fixed << setprecision(y) << x;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sortt(x) sort(all(x))
+#define sorttr(x) sort(rall(x))
+#define mems(a, x) memset(a, x, sizeof(a))
+#define rev(x) reverse(all(x))
+#define co cout <<
+#define inp(x) cin >> x
+#define space cout << " "
+#define ln cout << '\n'
+#define takevect(x) for (auto &d : x) cin >> d;
+#define len(s) (int)s.size()
+#define rep(i, y) for (int i = 0; i < y; i++)
+#define INF INT_MAX
+ 
+ll ar[100001] , dp[100001];
+ 
+class solution {
+public:
+    void solve() {
+        int n ; cin >> n ;
+        string s,ans = ""; cin >> s;
+        for(auto d : s){
+            if(d == '4'){
+                ans += '3' ;ans += '2' ;ans += '2' ;
+            }else if(d == '8'){
+                ans += '7' ;ans += '2' ;ans += '2' ;ans += '2';
+            }else if(d == '9') {
+                ans += '7' ;ans += '3' ;ans += '3' ;ans += '2';
+            }else if(d == '6'){
+                ans += '5';ans +='3';
+            }else if(d == '0' || d == '1') {
+                continue;
+            }else{
+                ans += d;
+            }
+        }
+        sorttr(ans);
+        co ans;
+    }
+};
+ 
+signed main() {
+    fast;
+    cout << fixed;
+    cout << setprecision(14);
+    #ifndef ONLINE_JUDGE
+        freopen("inp.txt", "r", stdin);
+        freopen("out.txt", "w", stdout);
+    #endif
+    solution sol;
+    int test=1 ;
+    // inp ( test ); 
+    while ( test -- ) sol.solve();
+ 
+	
+    return 0;
+}
